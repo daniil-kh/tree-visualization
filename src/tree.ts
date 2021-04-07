16a828d7e49d84d7d0ca0ecc91e02acc2d058185
daniil-kh <daniil.khmurovich@gmail.com>
2021-04-07 15:10:29 +0300

    refactor file structure

--- src/tree.ts
+++ src/tree.ts
@@ -1,10 +1,22 @@
 abstract class INode {
-  protected _right: INode;
-  protected _left: INode;
-  protected _parent: INode;
-  abstract clone(): INode;
-  abstract equal(node: INode): boolean;
-  abstract compare(node: INode): number;
+  protected abstract _right: INode;
+  protected abstract _left: INode;
+  protected abstract _parent: INode;
+
+  public abstract clone(): INode;
+  public abstract equal(node: INode): boolean;
+  public abstract compare(node: INode): number;
+  public abstract copy(node: INode): void;
+  public abstract content(): unknown;
+
+  public abstract get parent(): INode;
+  public abstract set parent(node: INode);
+
+  public abstract get right(): INode;
+  public abstract set right(node: INode);
+
+  public abstract get left(): INode;
+  public abstract set left(node: INode);
 }
 
 class BNode<T> extends INode {
@@ -21,42 +33,33 @@ class BNode<T> extends INode {
     this._data = data;
   }
 
-  // need to refactor this part
-  // begin of part
-
-  public get parent() {
+  public get parent(): BNode<T> {
     return this._parent;
   }
-
-  public get right() {
-    return this._right;
-  }
-
-  public get left() {
-    return this._left;
-  }
-
-  public get data() {
-    return this._data;
-  }
-
   public set parent(node: BNode<T>) {
     this._parent = node;
   }
 
-  public set right(node: BNode<T>) {
-    this._right = node;
+  public get left(): BNode<T> {
+    return this._left;
   }
-
   public set left(node: BNode<T>) {
     this._left = node;
   }
 
+  public get right(): BNode<T> {
+    return this._right;
+  }
+  public set right(node: BNode<T>) {
+    this._right = node;
+  }
+
+  public get data(): T {
+    return this._data;
+  }
   public set data(data: T) {
     this._data = data;
   }
-
-  //end of part
 
   public clone(): BNode<T> {
     let newNode = new BNode<T>(this._data);
@@ -80,23 +83,63 @@ class BNode<T> extends INode {
       return 0;
     }
   }
+
+  public copy(node: BNode<T>): void {
+    this._data = node._data;
+  }
+
+  public content(): T {
+    return this._data;
+  }
 }
 
-class RBNode<T> extends BNode<T> {
+class RBNode<T> extends INode {
   protected _parent: RBNode<T>;
   protected _right: RBNode<T>;
   protected _left: RBNode<T>;
+  protected _data: T;
   protected _color: boolean;
 
   constructor(data: T = null) {
-    super(data);
+    super();
+    this._parent = null;
+    this._right = null;
+    this._left = null;
+    this._data = data;
     this._color = true;
+  }
+
+  public get parent(): RBNode<T> {
+    return this._parent;
+  }
+  public set parent(node: RBNode<T>) {
+    this._parent = node;
+  }
+
+  public get left(): RBNode<T> {
+    return this._left;
+  }
+  public set left(node: RBNode<T>) {
+    this._left = node;
+  }
+
+  public get right(): RBNode<T> {
+    return this._right;
+  }
+  public set right(node: RBNode<T>) {
+    this._right = node;
+  }
+
+  public get data() {
+    return this._data;
+  }
+  public set data(data: T) {
+    this._data = data;
   }
 
   public get color() {
     return this._color;
   }
-
   public set color(color: boolean) {
     this._color = color;
   }
@@ -113,6 +156,28 @@ class RBNode<T> extends BNode<T> {
     newNode._color = this._color;
 
     return newNode;
+  }
+
+  public equal(node: RBNode<T>): boolean {
+    return this._data === node._data;
+  }
+
+  public compare(node: RBNode<T>): number {
+    if (this._data > node._data) {
+      return 1;
+    } else if (this._data < node._data) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+
+  public copy(node: RBNode<T>) {
+    this._data = node._data;
+  }
+
+  public content(): T {
+    return this._data;
   }
 }
 
@@ -153,11 +218,11 @@ interface ITree {
 }
 
 class BTree<T> implements ITree {
-  protected root: BNode<T>;
+  protected root: INode;
   constructor() {
     this.root = null;
   }
-  public add(node: BNode<T>): void {
+  public add(node: INode): void {
     if (this.root === null) {
       this.root = node;
       node.parent = this.root;
@@ -167,7 +232,7 @@ class BTree<T> implements ITree {
     this.addNode(this.root, node);
   }
 
-  protected addNode(currentNode: BNode<T>, insetrtedNode: BNode<T>): void {
+  protected addNode(currentNode: INode, insetrtedNode: INode): void {
     if (currentNode.compare(insetrtedNode) > 0) {
       if (currentNode.left === null) {
         currentNode.left = insetrtedNode;
@@ -186,8 +251,8 @@ class BTree<T> implements ITree {
     }
   }
 
-  protected deleteNode(node: BNode<T>): void {
-    let nodeToDelete: BNode<T> = this.findNode(this.root, node);
+  protected deleteNode(node: INode): void {
+    let nodeToDelete: INode = this.findNode(this.root, node);
     if (nodeToDelete.equal(this.root)) {
       let nodeToExchange = this.findMax(nodeToDelete.left);
       if (nodeToExchange.left !== null) {
@@ -198,7 +263,7 @@ class BTree<T> implements ITree {
       } else {
         nodeToExchange.parent.left = nodeToExchange.left;
       }
-      nodeToDelete.data = nodeToExchange.data;
+      nodeToDelete.copy(nodeToExchange);
 
       return;
     }
@@ -212,7 +277,7 @@ class BTree<T> implements ITree {
       }
     } else if (nodeToDelete.left !== null) {
       let nodeToExchange = this.findMax(nodeToDelete.left);
-      nodeToDelete.data = nodeToExchange.data;
+      nodeToDelete.copy(nodeToExchange);
       if (nodeToExchange.left !== null) {
         nodeToExchange.left.parent = nodeToExchange.parent;
       }
@@ -226,33 +291,25 @@ class BTree<T> implements ITree {
     }
   }
 
-  public delete(node: BNode<T>): void {
+  public delete(node: INode): void {
     this.deleteNode(node);
   }
 
-  public find(node: BNode<T>): BNode<T> {
-    return this.findNode(this.root, node);
+  public find(node: INode): INode {
+    return this.findNode(this.root, node) as BNode<T>;
   }
-  protected findNode(currentNode: BNode<T>, findedNode: BNode<T>): BNode<T> {
+  protected findNode(currentNode: INode, findedNode: INode): INode {
     if (currentNode === null) return null;
     if (currentNode.equal(findedNode)) {
       return currentNode;
     } else if (currentNode.compare(findedNode) > 0) {
       return this.findNode(currentNode.left, findedNode);
-    } else if (currentNode.data <= findedNode.data) {
+    } else if (currentNode.compare(findedNode) <= 0) {
       return this.findNode(currentNode.right, findedNode);
     }
   }
 
-  private findMin(currentNode: BNode<T>): BNode<T> {
-    if (currentNode.left === null) {
-      return currentNode;
-    }
-
-    return this.findMin(currentNode.left);
-  }
-
-  protected findMax(currentNode: BNode<T>): BNode<T> {
+  protected findMax(currentNode: INode): INode {
     if (currentNode.right === null) {
       return currentNode;
     }
@@ -264,19 +321,19 @@ class BTree<T> implements ITree {
     this.showTree(this.root, 1);
   }
 
-  protected showTree(node: BNode<T>, height: number): void {
+  protected showTree(node: INode, height: number): void {
     if (node === null) {
       return;
     }
     this.showTree(node.left, height + 1);
-    let output: string = "-".repeat(height) + ">" + node.data;
+    let output: string = "-".repeat(height) + ">" + node.content();
     console.log(output);
     this.showTree(node.right, height + 1);
   }
 }
 
 class AVLTree<T> extends BTree<T> {
-  public add(node: BNode<T>) {
+  public add(node: INode) {
     if (this.root === null) {
       this.root = node;
       return;
@@ -286,7 +343,7 @@ class AVLTree<T> extends BTree<T> {
     this.checkBalanceFactor(this.findNode(this.root, node));
   }
 
-  private checkBalanceFactor(node: BNode<T>): void {
+  private checkBalanceFactor(node: INode): void {
     if (node.equal(this.root)) {
       this.fixUnbalanceDomain(node);
       return;
@@ -296,7 +353,7 @@ class AVLTree<T> extends BTree<T> {
     this.checkBalanceFactor(node.parent);
   }
 
-  private fixUnbalanceDomain(node: BNode<T>): void {
+  private fixUnbalanceDomain(node: INode): void {
     let heightLeft: number = this.findHeight(node.left, 1);
     let heightRight: number = this.findHeight(node.right, 1);
     if (Math.abs(heightLeft - heightRight) > 1) {
@@ -322,7 +379,7 @@ class AVLTree<T> extends BTree<T> {
     }
   }
 
-  private findHeight(node: BNode<T>, height: number): number {
+  private findHeight(node: INode, height: number): number {
     if (node === null) {
       return height - 1;
     }
@@ -332,7 +389,7 @@ class AVLTree<T> extends BTree<T> {
     return left >= right ? left : right;
   }
 
-  protected rightRotation(node: BNode<T>): void {
+  protected rightRotation(node: INode): void {
     if (node.equal(this.root)) {
       this.root = node.left;
     }
@@ -352,7 +409,7 @@ class AVLTree<T> extends BTree<T> {
     node.parent.right = node;
   }
 
-  protected leftRotation(node: BNode<T>): void {
+  protected leftRotation(node: INode): void {
     if (node.equal(this.root)) {
       this.root = node.right;
     }
@@ -372,17 +429,17 @@ class AVLTree<T> extends BTree<T> {
     node.parent.left = node;
   }
 
-  protected leftRightRotation(node: BNode<T>): void {
+  protected leftRightRotation(node: INode): void {
     this.leftRotation(node.left);
     this.rightRotation(node);
   }
 
-  protected rightLeftRotation(node: BNode<T>): void {
+  protected rightLeftRotation(node: INode): void {
     this.rightRotation(node.right);
     this.leftRotation(node);
   }
 
-  private checkAllNodes(currentNode: BNode<T>): void {
+  private checkAllNodes(currentNode: INode): void {
     if (currentNode === null) return;
 
     this.checkAllNodes(currentNode.left);
@@ -391,40 +448,203 @@ class AVLTree<T> extends BTree<T> {
     this.checkBalanceFactor(currentNode);
   }
 
-  public delete(node: BNode<T>): void {
+  public delete(node: INode): void {
     this.deleteNode(node);
     this.checkAllNodes(this.root);
   }
 }
 
-class RBTree<T> extends AVLTree<T> {
+class RBTree<T> extends BTree<T> {
   protected root: RBNode<T>;
+  constructor() {
+    super();
+    this.root = null;
+  }
+
   public add(node: RBNode<T>): void {
     if (this.root === null) {
       this.root = node;
+      this.root.color = false;
       return;
     }
 
     this.addNode(this.root, node);
-    this.checkRBRules(this.findNode(this.root, node) as RBNode<T>);
+    this.balanceTree(this.findNode(this.root, node) as RBNode<T>);
   }
-  protected checkRBRules(node: RBNode<T>) {}
+  protected balanceTree(node: RBNode<T>): void {
+    if (node?.equal(this.root)) {
+      return;
+    }
 
-  protected checkColor(node: RBNode<T>) {}
+    let nodeGrandparent = node.parent.parent;
+    this.balanceSubTree(node);
+    if (nodeGrandparent !== null) this.balanceTree(nodeGrandparent);
+    this.root.color = false;
+  }
+
+  protected balanceSubTree(node: RBNode<T>): void {
+    if (node.equal(this.root)) {
+      this.root.color = false;
+      return;
+    }
+
+    if (node.parent.color === node.color && node.parent.parent !== null) {
+      if (node.parent.parent.left?.equal(node.parent)) {
+        if (
+          node.parent.parent.right !== null &&
+          node.parent.parent.right.color === true
+        ) {
+          this.recolor(node);
+        } else {
+          this.rotateSubtree(node);
+        }
+      } else {
+        if (
+          node.parent.parent.left !== null &&
+          node.parent.parent.left.color === true
+        ) {
+          this.recolor(node);
+        } else {
+          this.rotateSubtree(node);
+        }
+      }
+    }
+  }
+
+  protected recolor(node: RBNode<T>): void {
+    if (node.parent.parent?.left.equal(node.parent)) {
+      if (node.parent.parent.right !== null) {
+        node.parent.parent.right.color = false;
+      }
+    } else {
+      if (node.parent.parent.left !== null) {
+        node.parent.parent.left.color = false;
+      }
+    }
+    node.parent.color = false;
+    node.parent.parent.color = true;
+  }
+
+  protected rotateSubtree(node: RBNode<T>): void {
+    if (node.parent.left?.equal(node)) {
+      if (node.parent.parent.left?.equal(node.parent)) {
+        this.rightRotation(node.parent.parent);
+        node.parent.colorFlip();
+        node.parent.right.colorFlip();
+      } else {
+        this.rightLeftRotation(node.parent.parent);
+        node.colorFlip();
+        node.left.colorFlip();
+      }
+    } else {
+      if (node.parent.parent.left?.equal(node.parent)) {
+        this.leftRightRotation(node.parent.parent);
+        node.colorFlip();
+        node.right.colorFlip();
+      } else {
+        this.leftRotation(node.parent.parent);
+        node.parent.colorFlip();
+        node.parent.left.colorFlip();
+      }
+    }
+  }
+
+  protected rightRotation(node: RBNode<T>): void {
+    if (node.equal(this.root)) {
+      this.root = node.left;
+    }
+    if (node.parent !== null) {
+      if (node.parent.left?.equal(node)) {
+        node.parent.left = node.left;
+      } else {
+        node.parent.right = node.left;
+      }
+    }
+    node.left.parent = node.parent;
+    node.parent = node.left;
+    node.left = node.left.right;
+    if (node.left !== null) {
+      node.left.parent = node;
+    }
+    node.parent.right = node;
+  }
+
+  protected leftRotation(node: RBNode<T>): void {
+    if (node.equal(this.root)) {
+      this.root = node.right;
+    }
+    if (node.parent !== null) {
+      if (node.parent.left?.equal(node)) {
+        node.parent.left = node.right;
+      } else {
+        node.parent.right = node.right;
+      }
+    }
+    node.right.parent = node.parent;
+    node.parent = node.right;
+    node.right = node.right.left;
+    if (node.right !== null) {
+      node.right.parent = node;
+    }
+    node.parent.left = node;
+  }
+
+  protected leftRightRotation(node: RBNode<T>): void {
+    this.leftRotation(node.left);
+    this.rightRotation(node);
+  }
+
+  protected rightLeftRotation(node: RBNode<T>): void {
+    this.rightRotation(node.right);
+    this.leftRotation(node);
+  }
 
   public delete(node: RBNode<T>): void {
     this.deleteNode(node);
   }
 
-  public find(node: RBNode<T>): RBNode<T> {
+  public find(node: INode): RBNode<T> {
     return this.findNode(this.root, node) as RBNode<T>;
+  }
+
+  protected showTree(node: RBNode<T>, height: number): void {
+    if (node === null) {
+      return;
+    }
+    this.showTree(node.left, height + 1);
+    let output: string =
+      (node.color ? "\x1b[31m" : "\x1b[30m") +
+      "-".repeat(height) +
+      ">" +
+      node.content() +
+      "\x1b[0m";
+    console.log(output);
+    this.showTree(node.right, height + 1);
   }
 }
 
 const bTreeExample = (): void => {
-  const tree = new BTree<number>();
-  const array = [8, 7, 6, 6, 4, 5, 20, 25, 15, 16, 14, 24, 24, 23, 23, 22, 22];
-  array.forEach((element) => {
+  const tree: BTree<number> = new BTree<number>();
+  const array: Array<number> = [
+    8,
+    7,
+    6,
+    6,
+    4,
+    5,
+    20,
+    25,
+    15,
+    16,
+    14,
+    24,
+    24,
+    23,
+    23,
+    22,
+    22,
+  ];
+  array.forEach((element: number): void => {
     tree.add(new BNode<number>(element));
   });
 
@@ -435,8 +655,8 @@ const bTreeExample = (): void => {
 };
 
 const avlTreeExample = (): void => {
-  const avlTree = new AVLTree<number>();
-  const array = [
+  const avlTree: AVLTree<number> = new AVLTree<number>();
+  const array: Array<number> = [
     2,
     17,
     16,
@@ -458,7 +678,7 @@ const avlTreeExample = (): void => {
     11,
     10,
   ];
-  array.forEach((element) => {
+  array.forEach((element: number): void => {
     avlTree.add(new BNode<number>(element));
   });
 
@@ -468,4 +688,36 @@ const avlTreeExample = (): void => {
   avlTree.show();
 };
 
-avlTreeExample();
+const rbTreeExample = (): void => {
+  const rbTree: RBTree<number> = new RBTree<number>();
+  const array: Array<number> = [
+    12,
+    11,
+    6,
+    9,
+    2,
+    7,
+    13,
+    19,
+    15,
+    4,
+    18,
+    17,
+    16,
+    1,
+    10,
+    20,
+    8,
+    14,
+    3,
+    5,
+  ];
+  //const array: Array<number> = [1, 2, 3];
+  array.forEach((element: number): void => {
+    rbTree.add(new RBNode<number>(element));
+  });
+
+  rbTree.show();
+};
+
+rbTreeExample();
